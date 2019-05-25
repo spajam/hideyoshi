e9d9d081fa870af572f494102a2ed6a407c42e01
wadai-tanaka <30337102+wadai-tanaka@users.noreply.github.com>
2019-05-25 16:05:53 +0900

    Test

---
+++ hideyoshi/Assets/Scenes/_Kazuhiro/ChagukiManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChagukiManager : MonoBehaviour
+{
+    
+    float Chaguki_rad=0.2f;
+    [SerializeField]
+    GameObject Chawan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Touch touch = Input.GetTouch(1);
+        Vector3 touchpos=Input.mousePosition;
+    }
+}
