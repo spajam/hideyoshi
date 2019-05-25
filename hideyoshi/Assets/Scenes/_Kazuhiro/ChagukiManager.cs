1b21f32e6012d38e24daef9d6a18a50915c63946
wadai-tanaka <30337102+wadai-tanaka@users.noreply.github.com>
2019-05-25 16:58:28 +0900

    1

--- hideyoshi/Assets/Scenes/_Kazuhiro/ChagukiManager.cs
+++ hideyoshi/Assets/Scenes/_Kazuhiro/ChagukiManager.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class ChagukiManager : MonoBehaviour
 {
-    
-    float Chaguki_rad=0.2f;
+    Vector3 LastPos;
     [SerializeField]
-    GameObject Chawan;
+    Camera _camera;
+    Vector3 Motutoko;
+    float Chaguki_rad=2f;
+    [SerializeField]
+    Sprite[] ChawanImage;
+    [SerializeField]
+    GameObject Chawan,ChagukiAnchor;
+    Transform Chaguki;
+    [SerializeField]
+    AudioClip Shake;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+      //  Touch touch = Input.GetTouch(1);
+        Chawan.GetComponent<SpriteRenderer>().sprite = ChawanImage[0];
+        Chaguki = ChagukiAnchor.transform.parent;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Touch touch = Input.GetTouch(1);
-        Vector3 touchpos=Input.mousePosition;
+        
+
+    }
+
+    public void HoldChaguki() {
+        Debug.Log(_camera.ScreenToWorldPoint(Input.mousePosition));
+        Vector3 touchpos = _camera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 deltaPos = LastPos - touchpos;
+        Chaguki.transform.position = new Vector3(touchpos.x, touchpos.y,0);
+        LastPos = touchpos;
     }
 }
