ee7d09fdacae8e73350c06a7bbd96e4c53263b58
OmoriItsuki <39805387+kinoko555@users.noreply.github.com>
2019-05-25 21:56:42 +0900

    きたばのフォルダを追加

---
+++ hideyoshi/Assets/Scenes/Yuga/Parameters.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// シーン間の変数
+/// </summary>
+public class Parameters : MonoBehaviour
+{
+    public static int owan;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
