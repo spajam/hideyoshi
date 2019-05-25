387f7443b3aff9e682e49c2e5151a15062e7b0ee
OmoriItsuki <39805387+kinoko555@users.noreply.github.com>
2019-05-25 18:55:28 +0900

    アニメーション完成

--- hideyoshi/Assets/omoriscript/result_cal.cs
+++ hideyoshi/Assets/omoriscript/result_cal.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class result_cal : MonoBehaviour
 {
+    public Text point;
+    public Text grade;
     // Start is called before the first frame update
     void Start()
     {
-        
+        point = GetComponent<Text>();
+        grade = GetComponent<Text>();
     }
 
     // Update is called once per frame
