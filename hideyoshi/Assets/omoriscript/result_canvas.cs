e805ec76dbd8f53ac239cb729e1b21bc2b39989d
OmoriItsuki <39805387+kinoko555@users.noreply.github.com>
2019-05-25 18:14:10 +0900

    アニメーション追加

---
+++ hideyoshi/Assets/omoriscript/result_canvas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class result_canvas : MonoBehaviour
+{
+    public Vector2 SPEED = new Vector2(0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < 800)
+        {
+            Move();
+        }
+    }
+    
+    private void Move()
+    {
+        Vector2 Position = transform.position;
+        this.transform.position += Vector3.up * SPEED.y;
+    }
+}
