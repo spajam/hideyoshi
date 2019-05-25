49f677d7a63d303799f2f4b5a8a66fb75705df80
OmoriItsuki <39805387+kinoko555@users.noreply.github.com>
2019-05-26 02:29:42 +0900

    ボタン追加

---
+++ hideyoshi/Assets/omoriscript/toTitle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class toTitle : MonoBehaviour
+{
+    public void Ontouch()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                SceneManager.LoadScene("Title");
+            }
+        }
+    }
+}
