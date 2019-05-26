620f81c0590bdfdcaf39d8cf0a30e0ca5376b147
Yuga <45866014+YugaKitabayashi@users.noreply.github.com>
2019-05-26 09:56:02 +0900

    セレクト前タイトル追加

---
+++ hideyoshi/Assets/Scenes/Yuga/PreTitle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreTitle : MonoBehaviour
+{
+    public void ToTitle()
+    {
+        SceneManager.LoadSceneAsync("Select");
+    }
+}
